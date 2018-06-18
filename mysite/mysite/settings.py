41775523b4a14382b545924765909ea8482884d7
zjjfx88 <zjj7842806@163.com>
2018-06-18 13:39:27 +0800

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = "/search/odin/pypro/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
